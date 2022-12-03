1d3f6cd2103e89da94b4dd5d4387b97d3d038101
Pablo Ferreiro <pferreiromero@gmail.com>
2022-12-03 09:38:35 +0100

    Estructura DB

--- tools/structure.sql
+++ tools/structure.sql
@@ -1,11 +1,13 @@
 -- wikiuma.reviews definition
 CREATE TABLE `reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idnc` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `to` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT 'Anon',
   `note` float NOT NULL,
   `message` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `votes` int(11) DEFAULT 0,
+  `subject` tinyint(1),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
