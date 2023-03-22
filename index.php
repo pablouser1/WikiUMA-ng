c243699bc9628b09b224c6d81f7f02c712b45102
Pablo Ferreiro <pferreiromero@gmail.com>
2023-03-22 16:50:22 +0100

    moved from public

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-require __DIR__ . "/../vendor/autoload.php";
-require __DIR__ . "/../bootstrap.php";
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/bootstrap.php";
 
 // ROUTER
 $router = new Bramus\Router\Router();
 $router->setNamespace('\App\Controllers');
 
-require __DIR__ . '/../routes.php';
+require __DIR__ . '/routes.php';
 
 session_start();
 
