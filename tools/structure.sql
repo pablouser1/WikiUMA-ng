7ac4768cc9ee38ac254b26bd06fb1f344829b09d
Pablo Ferreiro <me@pabloferreiro.es>
2023-04-26 20:38:17 +0200

    proper structure

--- tools/structure.sql
+++ tools/structure.sql
@@ -1,15 +1,48 @@
 -- wikiuma.reviews definition
 CREATE TABLE `reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `to` varchar(36) NOT NULL,
+  `data` varchar(36) NOT NULL,
   `username` varchar(16) DEFAULT 'Anon',
   `note` float NOT NULL,
   `message` text DEFAULT NULL,
   `votes` int(11) DEFAULT 0,
-  `subject` tinyint(1),
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
+  `subject` tinyint(1) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- wikiuma.tags definition
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `icon` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tags_UN` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- wikiuma.users definition
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `niu` varchar(16) NOT NULL,
+  `password` binary(60) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_UN` (`niu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- wikiuma.verify definition
+CREATE TABLE `verify` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `niu` varchar(16) NOT NULL,
+  `code` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `verify_UN` (`niu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- wikiuma.reports definition
 CREATE TABLE `reports` (
@@ -21,15 +54,6 @@ CREATE TABLE `reports` (
   CONSTRAINT `reports_FK` FOREIGN KEY (`review_id`) REFERENCES `reviews` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Reportes hechos por el usuario';
 
--- wikiuma.tags definition
-CREATE TABLE `tags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 0,
-  `icon` varchar(4) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `tags_UN` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- wikiuma.reviews_tags definition
 CREATE TABLE `reviews_tags` (
@@ -39,23 +63,4 @@ CREATE TABLE `reviews_tags` (
   KEY `reviews_tags_FK_1` (`tag_id`),
   CONSTRAINT `reviews_tags_FK` FOREIGN KEY (`review_id`) REFERENCES `reviews` (`id`) ON DELETE CASCADE,
   CONSTRAINT `reviews_tags_FK_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- wikiuma.users definition
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `niu` varchar(16) NOT NULL,
-  `password` binary(60) NOT NULL,
-  `admin` tinyint(1),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_UN` (`niu`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- wikiuma.verify definition
-CREATE TABLE `verify` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `niu` varchar(16) NOT NULL,
-  `code` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `verify_UN` (`niu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
