ef8f04d921e1c573a76488f6ba261dee24ee2124
Pablo Ferreiro <me@pabloferreiro.es>
2025-09-20 20:20:15 +0200

    better review created

--- templates/views/report-created.php
+++ templates/views/report-created.php
@@ -9,15 +9,15 @@
             <?php if ($report->email !== null): ?>
                 Se te enviará un correo electrónico cuando la administración tome una decisión.
             <?php else: ?>
-                Si quieres saber el estado de tu informe, puedes enviar un correo electrónico a <b><?= $this->contact() ?></b>.
-                Recuerda especificar tu ID.
+                Si quieres saber el estado de tu informe, puedes enviar un correo electrónico, especificando tu ID, a
+                <a href="mailto:<?= $this->contact() ?>"><?= $this->contact() ?>
             <?php endif ?>
         </p>
 
-        <article class="message is-success">
-            <div class="message-body">Gracias por hacer de WikiUMA un espacio más cívico y respetuoso <span style="color: #e25555;">&#9829;</span></div>
-        </article>
-
         <a href="<?= $back ?>" class="button">Atrás</a>
     </div>
+
+    <article class="message is-success">
+        <div class="message-body">Gracias por hacer de WikiUMA un espacio más cívico y respetuoso <span style="color: #e25555;">&#9829;</span></div>
+    </article>
 </div>
