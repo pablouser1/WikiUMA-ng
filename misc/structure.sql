2f5e43c947ffc1af6b7d67635e41bae7a7c030fe
Pablo Ferreiro <me@pabloferreiro.es>
2025-09-21 17:55:11 +0200

    updated struct

--- misc/structure.sql
+++ misc/structure.sql
@@ -4,7 +4,7 @@ CREATE TABLE `reviews` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
   `target` varchar(36) NOT NULL,
-  `username` varchar(16) DEFAULT 'Anon',
+  `username` varchar(16) DEFAULT NULL,
   `note` float NOT NULL,
   `message` text DEFAULT NULL,
   `votes` int(11) DEFAULT 0,
@@ -19,18 +19,34 @@ CREATE TABLE `tags` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
   `name` varchar(32) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 0,
-  `for`  tinyint(1) NOT NULL DEFAULT 0,
+  `type` tinyint(1) NOT NULL,
+  `for` tinyint(1) NOT NULL,
   `icon` varchar(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tags_UN` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- wikiuma.users definition
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` binary(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_unique` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- wikiuma.reports definition
 CREATE TABLE `reports` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `uuid` uuid NOT NULL,
   `review_id` int(11) NOT NULL,
   `message` text NOT NULL,
   `reason` text DEFAULT NULL,
