7a84a9bceb1ce60407ce6a5dd296449f725e88e7
Pablo Ferreiro <me@pabloferreiro.es>
2025-09-23 13:27:06 +0200

    split routes

--- public/index.php
+++ public/index.php
@@ -30,47 +30,7 @@ $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
 
 $router = new League\Route\Router;
 
-// -- ROUTES -- //
-$router->get('/', [HomeController::class, 'index']);
-$router->get('/about', [MiscController::class, 'about']);
-$router->get('/legal', [MiscController::class, 'legal']);
-$router->get('/redirect', [RedirectController::class, 'index']);
-$router->get('/search', [SearchController::class, 'index']);
-
-$router->group('/reports', function (RouteGroup $route) {
-    $route->get('/', [ReportsController::class, 'index']);
-    $route->post('/', [ReportsController::class, 'post']);
-});
-
-$router->get('/centros', [CentrosController::class, 'index']);
-$router->get('/centros/{centro_id:number}/titulaciones', [TitulacionesController::class, 'index']);
-$router->group('/planes/{plan_id:number}', function (RouteGroup $route) {
-    $route->get('/', [PlanesController::class, 'index']);
-    $route->get('/asignaturas/{asignatura_id:number}', [AsignaturasController::class, 'index']);
-});
-
-$router->get('/profesores', [ProfesoresController::class, 'index']);
-$router->get('/challenge', [ChallengeController::class, 'index']);
-
-$router->post('/reviews', [ReviewsController::class, 'create']);
-$router->group('/reviews/{review_id:number}', function (RouteGroup $route) {
-    $route->get('/report', [ReviewsController::class, 'reportIndex']);
-    $route->post('/report', [ReviewsController::class, 'reportCreate']);
-});
-
-$router->group('/staff', function (RouteGroup $route) {
-    $route->get('/', [StaffController::class, 'dashboard']);
-    $route->get('/login', [StaffController::class, 'loginGet']);
-    $route->post('/login', [StaffController::class, 'loginPost']);
-    $route->post('/reports/{report_id:number}/status', [StaffController::class, 'reportStatus']);
-})->middleware(new AuthMiddleware());
-
-if (Env::app_debug()) {
-    $router->group('/dev', function (RouteGroup $route) {
-        $route->get('/reactions/{code:number}', [DevController::class, 'reactions']);
-    });
-}
-// -- END ROUTES -- //
+require __DIR__ . '/../routes.php';
 
 $response = null;
 
---
+++ routes.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Controllers\AsignaturasController;
+use App\Controllers\CentrosController;
+use App\Controllers\ChallengeController;
+use App\Controllers\DevController;
+use App\Controllers\HomeController;
+use App\Controllers\MiscController;
+use App\Controllers\PlanesController;
+use App\Controllers\ProfesoresController;
+use App\Controllers\RedirectController;
+use App\Controllers\ReportsController;
+use App\Controllers\ReviewsController;
+use App\Controllers\SearchController;
+use App\Controllers\StaffController;
+use App\Controllers\TitulacionesController;
+use App\Middleware\AuthMiddleware;
+use App\Wrappers\Env;
+use League\Route\RouteGroup;
+
+/** @var League\Route\Router $router */
+
+$router->get('/', [HomeController::class, 'index']);
+$router->get('/about', [MiscController::class, 'about']);
+$router->get('/legal', [MiscController::class, 'legal']);
+$router->get('/redirect', [RedirectController::class, 'index']);
+$router->get('/search', [SearchController::class, 'index']);
+
+$router->group('/reports', function (RouteGroup $route) {
+    $route->get('/', [ReportsController::class, 'index']);
+    $route->post('/', [ReportsController::class, 'post']);
+});
+
+$router->get('/centros', [CentrosController::class, 'index']);
+$router->get('/centros/{centro_id:number}/titulaciones', [TitulacionesController::class, 'index']);
+$router->group('/planes/{plan_id:number}', function (RouteGroup $route) {
+    $route->get('/', [PlanesController::class, 'index']);
+    $route->get('/asignaturas/{asignatura_id:number}', [AsignaturasController::class, 'index']);
+});
+
+$router->get('/profesores', [ProfesoresController::class, 'index']);
+$router->get('/challenge', [ChallengeController::class, 'index']);
+
+$router->post('/reviews', [ReviewsController::class, 'create']);
+$router->group('/reviews/{review_id:number}', function (RouteGroup $route) {
+    $route->get('/report', [ReviewsController::class, 'reportIndex']);
+    $route->post('/report', [ReviewsController::class, 'reportCreate']);
+});
+
+$router->group('/staff', function (RouteGroup $route) {
+    $route->get('/', [StaffController::class, 'dashboard']);
+    $route->get('/login', [StaffController::class, 'loginGet']);
+    $route->post('/login', [StaffController::class, 'loginPost']);
+    $route->post('/reports/{report_id:number}/status', [StaffController::class, 'reportStatus']);
+})->middleware(new AuthMiddleware());
+
+if (Env::app_debug()) {
+    $router->group('/dev', function (RouteGroup $route) {
+        $route->get('/reactions/{code:number}', [DevController::class, 'reactions']);
+    });
+}
