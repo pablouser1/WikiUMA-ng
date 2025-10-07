10692602ac9edf37bb89ff1812ff0a684257f6f7
Pablo Ferreiro <me@pabloferreiro.es>
2025-10-07 13:26:56 +0200

    faq

--- templates/views/about.php
+++ templates/views/about.php
@@ -10,7 +10,35 @@
         <p>WikiUMA <small>ng</small> aspira a continuar el funcionamiento de su antecesor con varias mejoras.
     </div>
     <div class="content">
-        <p class="title">Créditos</p>
+        <h1>Preguntas frecuentes / Soporte</h1>
+
+        <h4>¿Qué datos se recopilan acerca de mi?</h4>
+        <p>WikiUMA recoge la siguiente información:</p>
+        <ul>
+            <li>Dirección IP <b>SÓLO</b> en los siguientes casos:</li>
+            <ul>
+                <li>Fallos constantes de captcha.</li>
+                <li>Gran cantidad de solicitudes continuadas.</li>
+                <li>En el resto de casos, tu dirección IP no se almacena.</li>
+            </ul>
+            <li>
+                Al crear un informe, el correo electrónico dado es almacenado y
+                usado exclusivamente para enviar actualizaciones de su estado.
+            </li>
+        </ul>
+
+        <h4>¿Dónde están las valoraciones de la antigua versión de WikiUMA?</h4>
+        <p>Actualmente esos datos no están importados en la versión actual.</p>
+        <p>
+            La estructura de estos datos son considerablemente distintos a la actual y
+            la migración sería costosa, tanto en tiempo invertido como en recursos.
+        </p>
+
+        <h4>Las palabras más usadas en las valoraciones de un profesor / una asignatura no coincide con los datos reales</h4>
+        <p>Estos datos no se actualizan en tiempo real, se calcula cada 7 días.</p>
+    </div>
+    <div class="content">
+        <h1>Cŕeditos</h1>
         <p>Este proyecto no sería posible sin la ayuda de:</p>
         <ul>
             <li>
