400c5f0e9fabeaba9303c1099411e156ab285b06
Pablo Ferreiro <me@pabloferreiro.es>
2025-11-11 17:47:13 +0100

    quote username

--- templates/views/mails/report-new.php
+++ templates/views/mails/report-new.php
@@ -1,5 +1,5 @@
 Hola <?= $this->e($user->first_name) ?>,
 
-Ha llegado un informe a WikiUMA al usuario <?= $this->e($report->review->username) ?> con el ID <?= $this->e($report->uuid) ?>.
+Ha llegado un informe a WikiUMA al usuario "<?= $this->e($report->review->username) ?>" con el ID <?= $this->e($report->uuid) ?>.
 
 Motivo: <?= $report->message ?>
