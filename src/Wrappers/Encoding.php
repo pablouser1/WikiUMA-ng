bb179bcc1c17e06beb584b57563af470625ff324
Pablo Ferreiro <me@pabloferreiro.es>
2025-12-07 22:04:49 +0100

    better base64 url functions

--- src/Wrappers/Encoding.php
+++ src/Wrappers/Encoding.php
@@ -7,32 +7,16 @@ class Encoding
     /**
      * Encode data to Base64URL
      */
-    public static function base64url_encode(string $data): bool|string
+    public static function base64url_encode(string $data): string
     {
-        // First of all you should encode $data to Base64 string
-        $b64 = base64_encode($data);
-
-        // Make sure you get a valid result, otherwise, return FALSE, as the base64_encode() function do
-        if ($b64 === false) {
-            return false;
-        }
-
-        // Convert Base64 to Base64URL by replacing “+” with “-” and “/” with “_”
-        $url = strtr($b64, '+/', '-_');
-
-        // Remove padding character from the end of line and return the Base64URL result
-        return rtrim($url, '=');
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
 
     /**
      * Decode data from Base64URL
      */
-    public static function base64url_decode(string $data, bool $strict = false): bool|string
+    public static function base64url_decode(string $data, bool $strict = false): string|false
     {
-        // Convert Base64URL to Base64 by replacing “-” with “+” and “_” with “/”
-        $b64 = strtr($data, '-_', '+/');
-
-        // Decode Base64 string and return the original data
-        return base64_decode($b64, $strict);
+        return base64_decode(strtr($data, '-_', '+/'), $strict);
     }
 }
