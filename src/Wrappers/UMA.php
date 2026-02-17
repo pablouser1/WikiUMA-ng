379441a81a02e44983ccde1fa6711d0f1d385cb3
Pablo Ferreiro <me@pabloferreiro.es>
2026-02-17 09:38:18 +0100

    cache nullable

--- src/Wrappers/UMA.php
+++ src/Wrappers/UMA.php
@@ -10,7 +10,7 @@ class UMA
     public static function api(): Api
     {
         $cache = Env::api_cache();
-        $options = new Options(cache: $cache->engine());
+        $options = new Options(cache: $cache?->engine());
         return new Api($options);
     }
 }
